cd9af44040e0066abd312185936be32d5cf74890
ansh589 <ansh32362@gmail.com>
2026-04-01 09:16:25 +0530

    updated PROGRAM2.C

--- Experiment-1/PEOGRAM2.C
+++ Experiment-1/PEOGRAM2.C
@@ -3,7 +3,7 @@ Create two functions MAX() and MIN(). MAX() is used to return the largest
 element and MIN() is used to return the smallest number in array. */
 #include <stdio.h>
 
-// Function to find maximum element
+
 int MAX(int arr[], int n) {
     int max = arr[0];
     for(int i = 1; i < n; i++) {
@@ -14,7 +14,7 @@ int MAX(int arr[], int n) {
     return max;
 }
 
-// Function to find minimum element
+
 int MIN(int arr[], int n) {
     int min = arr[0];
     for(int i = 1; i < n; i++) {
@@ -28,17 +28,17 @@ int MIN(int arr[], int n) {
 int main() {
     int arr[10];
 
-    // Input 10 numbers
+    
     printf("Enter 10 numbers:\n");
     for(int i = 0; i < 10; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Calling functions
+    
     int max = MAX(arr, 10);
     int min = MIN(arr, 10);
 
-    // Output
+    
     printf("Maximum number = %d\n", max);
     printf("Minimum number = %d\n", min);
 
